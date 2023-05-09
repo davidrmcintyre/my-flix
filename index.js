e951dbc50154b07c0dc491515b24137a2a20b880
David McIntyre <davidrobert.mcintyre@gmail.com>
2023-05-09 13:27:31 +0200

    bug fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ uuid = require('uuid'),
 app = express(),
 Models = require('./models.js');
 //mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
